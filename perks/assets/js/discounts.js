8e6b5e69842f4ee8d6a58e56dd5034501916886e
Ryan Bealey <rbealey@knights.ucf.edu>
2017-08-28 20:06:44 -0400

    Columns created to hold Canvs Perk offers.

--- perks/assets/js/discounts.js
+++ perks/assets/js/discounts.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	database.ref("perks").on("child_added", function(snapshot){
 		if (snapshot.val().approved === true){
 			$(".discounts")
-			.append("<div class='card' style='width: 20rem; margin-bottom: 10px;'>" +
+			.append("<div class='card organization' style='width: 20rem; margin-bottom: 10px;' category='" + snapshot.val().category + "'>" +
 						"<img class='card-img-top' src='" + snapshot.val().logo + "'>" +
 						"<div class='card-body'>" +
 							"<h4 class='card-title'>" + snapshot.val().corporation + "</h4>" +
